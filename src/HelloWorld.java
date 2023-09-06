7c6a09df9061f69544dd447e5a6eb4008fdcb954
Arlixm <m.alex2003@icloud.com>
2023-09-06 09:30:00 +0100

    .

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+* First program
+* @author Alex Moorhouse
+* @version 1
+*/
+
+
+
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        // Write Hello world to console
+        System.out.println("Hello world");
+    }
+}
