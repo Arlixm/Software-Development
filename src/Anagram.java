7bff86703cb367b616a8676e5e947a55ec551c80
Arlixm <m.alex2003@icloud.com>
2023-09-06 10:06:44 +0100

    .

---
+++ src/Anagram.java
@@ -0,0 +1,12 @@
+public class Anagram {
+
+    public static void main(String[] args) {
+        System.out.println("Here are 5 Anagrams, \n"+
+                "skiving = vikings, \n"+
+                "necessarian = renaissance, \n"+
+                "type = egypt, \n"+
+                "manors = romans, \n"+
+                "hoyt irs = history");
+
+    }
+}
---
+++ src/Haiku.java
@@ -0,0 +1,7 @@
+public class Haiku {
+    public static void main(String[] args) {
+        System.out.println("Ancient tales resound,\n" +
+                "Time's tapestry unfolds wide,\n" +
+                "Whispers of the past.");
+    }
+}
