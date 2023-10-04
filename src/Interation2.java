42a768c458c6e767d5770b4d402050f9e2dcc001
Arlixm <m.alex2003@icloud.com>
2023-10-04 09:10:08 +0100

    .

---
+++ src/Interation2.java
@@ -0,0 +1,9 @@
+public class Interation2 {
+    public static void main(String[] args) {
+        for (int count = 10; count >=0; count-- )
+            System.out.print("\n" + count);
+        {
+            System.out.print("\n" + "BLASTOFF!1!1!1!");
+        }
+    }
+}
