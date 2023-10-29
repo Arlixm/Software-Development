b273d8e3b158ec7a6b23aad8f6632dcf809bfb0a
Arlixm <143596732+Arlixm@users.noreply.github.com>
2023-10-29 01:30:49 +0100

    Add files via upload

---
+++ src/L2E2_Calculations.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class L2E2_Calculations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type in your first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Type in your second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println("What would you like to do with these two numbers? ");
+
+        System.out.println("1. Addition \n" + "2. Subtraction \n" + "3. Multiplication \n" + "4. Division ");
+        int ans = input.nextInt();
+        if (ans == 1){
+            System.out.print(num1 + num2);
+        }
+        if (ans == 2){
+            System.out.print(num1 - num2);
+        }
+        if (ans == 3){
+            System.out.print(num1 * num2);
+        }
+        if (ans == 4){
+            System.out.print(num1 / num2);
+        }
+
+    }
+
+}
