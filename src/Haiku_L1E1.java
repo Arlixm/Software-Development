98840f2f4698cb254699ef9ca603e657809609f5
Arlixm <143596732+Arlixm@users.noreply.github.com>
2023-11-22 10:04:20 +0000

    Delete src/Haiku_L1E1.java

--- src/Haiku_L1E1.java
+++
@@ -1,7 +0,0 @@
-public class Haiku_L1E1 {
-    public static void main(String[] args) {
-        System.out.println("Ancient tales resound,\n" +
-                "Time's tapestry unfolds wide,\n" +
-                "Whispers of the past.");
-    }
-}
