f1e41debee774173404a9569d99e70ce1a636659
Arlixm <143596732+Arlixm@users.noreply.github.com>
2023-11-22 10:04:14 +0000

    Delete src/Blastoff_L5E2.java

--- src/Blastoff_L5E2.java
+++
@@ -1,9 +0,0 @@
-public class Blastoff_L5E2 {
-    public static void main(String[] args) {
-        for (int count = 10; count >=0; count-- )
-            System.out.print("\n" + count);
-        {
-            System.out.print("\n" + "BLASTOFF!1!1!1!");
-        }
-    }
-}
